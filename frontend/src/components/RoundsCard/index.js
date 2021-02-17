44b752a3acc541e532eb416f505e04f7b65d194f
percist <percist@gmail.com>
2021-02-17 12:31:59 -0500

    styling on rounds card, add date

--- frontend/src/components/RoundsCard/index.js
+++ frontend/src/components/RoundsCard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {timeDifference} from '../../dateUtilities'
 
 const RoundsCard = ({ user, site, item, round, type }) => {
   const [receiver, setReceiver] = useState([])
@@ -24,23 +25,25 @@ const RoundsCard = ({ user, site, item, round, type }) => {
 
   return (
     <div className="rounds-card" >
-      <div className="rounds-card-info">
-        <div id="rounds-card-info_image">
+      <div className="rounds-card-header">
+        <div id="rounds-card-header_image">
           <a href={`/users/${user.id}`} >
             {!user.imgUrl && "Loading ..."}
             {user.imgUrl && <img src={user.imgUrl} alt="user" />}
           </a>
         </div>
-        <div className="rounds-card-info-description">
-          <div id="rounds-card-info_name">
+        <div className="rounds-card-header-description">
+          <div id="rounds-card-header_name">
             {user.username}
           </div>
-          <div id="rounds-card-info_caption">
-            {!item && <h2>loading....</h2>}
-            {item && `having a ${item.name}`}
+          <div id="rounds-card-header_time">
+            {timeDifference(round.createdAt)}
           </div>
         </div>
       </div>
+        <div id="rounds-card-header_comment">
+          {round.comment}
+        </div>
       <div className="rounds-card-image">
         {!round.imgUrl &&
           !site.imgUrl &&
@@ -54,23 +57,24 @@ const RoundsCard = ({ user, site, item, round, type }) => {
           <img src={site.imgUrl} alt="site"
           />}
       </div>
-      <div className="rounds-card-header">
-        <div id="rounds-card-header_site_name">
+      <div className="rounds-card-info">
+        <div id="rounds-card-info_site_name">
           {!site && <h2>loading....</h2>}
           {site &&
             <a href={`/sites/${site.id}`} >
               {site.name}
             </a>}
         </div>
-        <div id="rounds-card-header_site_location">
+        <div id="rounds-card-info_site_item">
+         {!item && <h2>loading....</h2>}
+          {item && `${item.name}`}
+        </div>
+        <div id="rounds-card-info_site_location">
           {!site && <h2>loading....</h2>}
           {site && `${site.city}, ${site.state}`}
         </div>
       </div>
-      <br color="#FFFFF" />
-      <div id="rounds-card-info_comment">
-        {round.comment}
-      </div>
+        <hr id="rounds-card-info_divider" color='silver'/>
     </div>
   )
 };
---
+++ frontend/src/dateUtilities.js
@@ -0,0 +1,32 @@
+export function timeDifference(inputTimeThen) {
+  const dateThen = new Date(inputTimeThen);
+  console.log(dateThen)
+  const dateNow = new Date();
+  console.log(dateNow)
+
+  let diffInSeconds = Math.abs(dateThen - dateNow) / 1000;
+
+  if (diffInSeconds >= 86400) {
+    // calculate days
+    const days = Math.floor(diffInSeconds / 86400);
+    diffInSeconds -= days * 86400;
+
+    return `${days}d`;
+
+  } else if (diffInSeconds >= 3600) {
+    // calculate hours
+    const hours = Math.floor(diffInSeconds / 3600) % 24;
+    diffInSeconds -= hours * 3600;
+
+    return `${hours}h`;
+
+  } else if (diffInSeconds >= 60) {
+    // calculate minutes
+    const minutes = Math.floor(diffInSeconds / 60) % 60;
+    diffInSeconds -= minutes * 60;
+
+    return `${minutes}m`
+
+  } else return 'Just now'
+
+}
