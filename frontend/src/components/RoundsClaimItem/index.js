291a1ae0b26d8b1fb725f64f2b977c712d4dc103
percist <percist@gmail.com>
2021-02-17 16:10:15 -0500

    styling done save buddies list

--- frontend/src/components/RoundsClaimItem/index.js
+++ frontend/src/components/RoundsClaimItem/index.js
@@ -60,62 +60,64 @@ const RoundsClaimRound = ({ round, roundsToDisplay, setRoundsToDisplay }) => {
 
   return (
     <div hidden={wasClaimed} className="rounds-claim-card">
+      <div className="rounds-claim-card-info">
 
         <div id="rounds-claim-card_image">
           <ItemImage image={item.imgUrl} />
         </div>
-      <div class="rounds-claim-card-details">
-        <div id="rounds-claim-card_info">
-          <div id="rounds-claim-card_info_round">
-            {!item && !site && <h2>loading...</h2>}
-            {item && site && 
-            <>
-              <h2>{item.name}</h2> 
-              <h2>at {site.name}</h2>
-            </>}
-          </div>
-          <div id="rounds-claim-card_info_sender">
-            Sent by {sender.username}
-          </div>
-        </div>
-        <div className="rounds-claim-card_actions">
-          <div className="rounds-claim-card_buttons">
-            <button hidden={wasClicked} onClick={handleRedeemClick} >
-              Redeem Now
-                        </button>
-            <button hidden={!wasClicked} onClick={handleNevermindClick} >
-              Maybe Later
-                        </button>
-            <div className="rounds-claim-qr">
-              <img hidden={!wasClicked} src={qrImage} alt="qr code" />
+        <div class="rounds-claim-card-details">
+          <div id="rounds-claim-card_info">
+            <div id="rounds-claim-card_info_round">
+              {!item && !site && <h2>loading...</h2>}
+              {item && site &&
+                <>
+                  <h2>{item.name}</h2>
+                  <a href={`/sites/${site.id}`}><h3>at {site.name}</h3></a>
+                </>}
+            </div>
+            <div id="rounds-claim-card_info_sender" >
+              <img src={sender.imgUrl}  alt="user" /> {sender.username}
             </div>
           </div>
-          <div hidden={!wasClicked} className="rounds-claim-card_comment">
-            <form className="comment-form_form" onSubmit={e => onCommentSubmit(e)}>
-              <div className="comment-form_form-field">
-                <label>
-                  Write a post:
-                </label>
-                <textarea 
-                  value={comment} 
-                  onChange={e => setComment(e.target.value)} 
-                  className="input-field" 
-                  />
-              </div>
-              <div className="comment-form_form-field">
-                <label>
-                  Add a photo:
-                </label>
-                  <input
-                    className="input menu-item-form_input"
-                    type="file"
-                    onChange={updateFile}
-                  />
-              </div>
-              <button type="submit" className="submit-button">
-                Post your Round
+        <button className="button redeem-button" hidden={wasClicked} onClick={handleRedeemClick} >
+          Redeem Now
                         </button>
-            </form>
+        </div>
+      </div>
+      <div className="rounds-claim-card_actions">
+        <div className="rounds-claim-qr">
+          <img hidden={!wasClicked} src={qrImage} alt="qr code" />
+        </div>
+        <div hidden={!wasClicked} className="rounds-claim-card_comment">
+          <form className="comment-form_form" onSubmit={e => onCommentSubmit(e)}>
+            <div className="comment-form_form-field">
+              <label>
+                Write a post:
+                </label>
+              <textarea
+                value={comment}
+                onChange={e => setComment(e.target.value)}
+                className="input rounds-cliam-form_input"
+              />
+            </div>
+            <div className="comment-form_form-field">
+              <label>
+                Add a photo:
+                </label>
+              <input
+                className="input menu-item-form_input"
+                type="file"
+                onChange={updateFile}
+              />
+            </div>
+            <button type="submit" className="button submit-button">
+              Post your Round
+                          </button>
+          </form>
+          <div className="comment-form_buttons">
+            <button className="button later-button" hidden={!wasClicked} onClick={handleNevermindClick} >
+              Maybe Later
+                          </button>
           </div>
         </div>
       </div>
